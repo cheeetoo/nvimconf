bcf029304104957c2a80f7fa23efc8a227eab280
cheeetoo <finnyjc12@gmail.com>
2022-12-16 14:18:16 -0600

    updated packer bootstrapping

--- lua/sussy/plugins.lua
+++ lua/sussy/plugins.lua
@@ -1,3 +1,15 @@
+-- bootstrapping
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
@@ -143,4 +155,9 @@ return require('packer').startup(function(use)
           require('sussy.plugins.zen-mode')
       end
     }
+
+    -- bootstrapping
+    if ensure_packer then
+        require('packer').sync()
+    end
 end)
